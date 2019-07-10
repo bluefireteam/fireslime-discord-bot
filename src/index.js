b63fc8478fc4a9b7272e4d5d64108ec7f946352b
Erick Zanardo <erick.zanardo@dextra-sw.com>
2019-07-10 09:27:20 -0300

    Adding error handling to prevent the bot from diying

--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ const Discord = require("discord.js")
 const { createWelcome }  = require("./welcome")
 const { createCommands } = require("./commands")
 
-const client = new Discord.Client()
+const startBot = () => {
+  const client = new Discord.Client()
 
-/* eslint-disable no-console */
-client.on("ready", () => console.log(`Logged in as ${client.user.tag}!`));
+  /* eslint-disable no-console */
+  client.on("ready", () => console.log(`Logged in as ${client.user.tag}!`));
 
-createWelcome(client);
-createCommands(client);
+  createWelcome(client);
+  createCommands(client);
 
-require("dotenv").config();
-client.login(process.env.BOT_TOKEN);
+  require("dotenv").config();
+  client.login(process.env.BOT_TOKEN);
+  client.on("error", e => {
+    console.error("Unexpected error", e);
+    startBot();
+  });
+}
+
+startBot();
